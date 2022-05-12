8b1147aa25e2bd9adcb1ec30e9d6fb30153a06b9
Anton Konovalov <extempl@pm.me>
2022-05-12 17:34:45 +0300

    Add max-warnings

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,15 @@ import { getInput, setFailed } from '@actions/core';
 async function run() {
 	try {
 		const customGlob = getInput('custom-glob');
+		const maxWarnings = getInput('max-warnings');
 		console.log(`##[add-matcher]${join(__dirname, '..', '.github', 'eslint-stylish.json')}`);
-		const args = [`${join(process.cwd(), 'node_modules/eslint/bin/eslint')}`, '--ext', 'js,jsx,ts,tsx', customGlob];
+		const args = [
+			`${join(process.cwd(), 'node_modules/eslint/bin/eslint')}`,
+			'--ext',
+			'js,jsx,ts,tsx',
+			`--max-warnings ${maxWarnings}`,
+			customGlob,
+		];
 		await exec('node', args);
 	} catch {
 		setFailed('');
